e21993b300745fa9b5c88f765cbf79e1edfefadd
BrunoMorais27 <bru.morais15@gmail.com>
2020-06-19 20:59:24 -0300

    atividade3

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,12 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require("readline-sync")
 
 // ## Faça o código ##
+
+let numQuad = rs.questionFloat("Qual o tamanho dos lados do quadrado? \n ")
+
+let resp = numQuad * 4 
+{ 
+    console.log ("o tamanho do seu quadrado é" + resp ) 
+}
